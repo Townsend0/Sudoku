1e26390fa229bdcf8ceddc31d73df1f23732d951
Townsend0 <126112061+Townsend0@users.noreply.github.com>
2023-02-26 15:38:56 +0300

    Add files via upload

--- sudoku.c
+++ sudoku.c
@@ -3,16 +3,16 @@
 #include<conio.h>
 #include<time.h>
 void PrintBoard(int **a);
-void AI(int **a);
+void AI(int **a,int (*m)[9]);
 int main(){
-    int **a=(int**)malloc(sizeof(int*)*9),b,c,*d,e[2],f,g=0,h,i,j,k,l;
+    int **a=(int**)malloc(sizeof(int*)*9),b,c,*d,e[2],f,g=0,h,i,j,k,l,m[9][9]={0};
     char s[5],t[9],r;
     for(b=0;b<9;b++)
-    a[b]=(int*)malloc(sizeof(int)*9);
+    a[b]=(int*)malloc(sizeof(int)*9);    
     srand(time(NULL));
     for(d=*a;d<=(*(a+8)+8);d++)
     *d=' ';
-    AI(a);
+    AI(a,m);
     for(b=38;b<1000;b++){
         PrintBoard(a);
         do{
@@ -29,7 +29,7 @@ int main(){
                 (s[1]<'1'||s[1]>'9')&&printf("\nEnter a number between 1 and 9 only")&&c++;
             }while(c);
             c=0;
-            (a[s[0]-49][s[1]-49]!=' ')&&printf("\nThe slot u picked is full")&&c++;
+            (m[s[0]-49][s[1]-49]!=0)&&printf("\nThe slot u picked can not be changed")&&c++;
         }while(c);
         do{
             c=0;
@@ -37,9 +37,9 @@ int main(){
             s[2]=getche();
             (s[2]<'1'||s[2]>'9')&&printf("\nThe value must be a number between 1 and 9")&&c++;
             for(f=0;f<9;f++)
-            (a[f][s[1]-49]==s[2])&&printf("\nThe value you enter already exists in the same column")&&c++;
+            (a[f][s[1]-49]==s[2]&&f!=s[0]-49)&&printf("\nThe value you enter already exists in the same column")&&c++;
             for(f=0;f<9;f++)
-            (a[s[0]-49][f]==s[2])&&printf("\nThe value you enter already exists in the same row")&&c++;
+            (a[s[0]-49][f]==s[2]&&f!=s[1]-49)&&printf("\nThe value you enter already exists in the same row")&&c++;
         }while(c);
         a[s[0]-49][s[1]-49]=s[2];
         system("cls");
@@ -54,7 +54,7 @@ int main(){
     getche();
     return 0;
 }
-void AI(int **a){
+void AI(int **a,int (*m)[9]){
     int b,c,f,h,i,j,k,g,x;
     for(b=0;b<38;b++){
         do{
@@ -74,6 +74,7 @@ void AI(int **a){
             }
         }while(g);
         a[c][f]=x;
+        m[c][f]=x;
     }
 }
 void PrintBoard(int **a){
